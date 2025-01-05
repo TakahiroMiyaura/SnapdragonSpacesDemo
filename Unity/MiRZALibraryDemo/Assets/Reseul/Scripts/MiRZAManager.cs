d515a1ad4ddfe8e5c9130fecbe497854de879f3f
Takahiro Miyaura <cafe.kobe@gmail.com>
2025-01-05 23:15:32 +0900

    MiRZA LibraryのMonitaringの制御を追加.

---
+++ Unity/MiRZALibraryDemo/Assets/Reseul/Scripts/MiRZAManager.cs
@@ -0,0 +1,110 @@
+// Copyright (c) 2025 Takahiro Miyaura
+// Released under the MIT license
+// http://opensource.org/licenses/mit-license.php
+
+using System;
+using System.Threading;
+using com.nttqonoq.devices.android.mirzalibrary;
+using TMPro;
+using UnityEngine;
+
+public class MiRZAManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI batteryLevelObj;
+
+    [SerializeField]
+    private TextMeshProUGUI chargeStatusObj;
+
+    [SerializeField]
+    private TextMeshProUGUI glassStatusObj;
+
+    [SerializeField]
+    private TextMeshProUGUI spacesModeStatusObj;
+
+    private SynchronizationContext mainThreadContext;
+    private MirzaPlugin mirzaPlugin;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        mainThreadContext = SynchronizationContext.Current;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+            // ÉâÉCÉuÉâÉäèâä˙âª
+            mirzaPlugin = new MirzaPlugin();
+#endif
+        mirzaPlugin?.SetLogEnable(true);
+    }
+
+    public void OnMonitoringStart()
+    {
+        if (mirzaPlugin == null)
+        {
+            return;
+        }
+
+        mirzaPlugin.OnBatteryLevelChanged += OnBatteryLevelChanged;
+        mirzaPlugin.OnDisplayStatusChanged += OnDisplayStatusChanged;
+        mirzaPlugin.OnGlassStatusChanged += OnGlassStatusChanged;
+        mirzaPlugin.OnGlassTouchGestureStatusChanged += OnGlassTouchGestureStatusChanged;
+        mirzaPlugin.OnPowerOffChanged += OnPowerOffChanged;
+        mirzaPlugin.OnServiceStateChanged += OnServiceStateChanged;
+        mirzaPlugin.OnSpacesModeStatusChanged += OnSpacesModeStatusChanged;
+
+        mirzaPlugin.StartMonitoring();
+    }
+
+    public void OnMonitoringStop()
+    {
+        if (mirzaPlugin == null)
+        {
+            return;
+        }
+
+        mirzaPlugin.StopMonitoring();
+
+        mirzaPlugin.OnBatteryLevelChanged -= OnBatteryLevelChanged;
+        mirzaPlugin.OnDisplayStatusChanged -= OnDisplayStatusChanged;
+        mirzaPlugin.OnGlassStatusChanged -= OnGlassStatusChanged;
+        mirzaPlugin.OnGlassTouchGestureStatusChanged -= OnGlassTouchGestureStatusChanged;
+        mirzaPlugin.OnPowerOffChanged -= OnPowerOffChanged;
+        mirzaPlugin.OnServiceStateChanged -= OnServiceStateChanged;
+        mirzaPlugin.OnSpacesModeStatusChanged -= OnSpacesModeStatusChanged;
+    }
+
+    private void OnSpacesModeStatusChanged(SpacesModeStatus obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnServiceStateChanged(ServiceState obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnPowerOffChanged()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnGlassTouchGestureStatusChanged(GlassTouchGestureStatus obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnGlassStatusChanged(GlassStatus obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDisplayStatusChanged(DisplayStatus obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnBatteryLevelChanged(int obj)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Unity/MiRZALibraryDemo/Assets/Reseul/Utilities/Scripts/FlatScreenModeDetectorForDualRenderFusion.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2024 Takahiro Miyaura
+// Released under the MIT license
+// http://opensource.org/licenses/mit-license.php
+
+using System.Collections.Generic;
+using MixedReality.Toolkit;
+using MixedReality.Toolkit.Input;
+using UnityEngine;
+
+namespace Reseul.Snapdragon.Spaces.Utilities
+{
+    internal class FlatScreenModeDetectorForDualRenderFusion : MonoBehaviour, IInteractionModeDetector
+    {
+        [SerializeField]
+        private List<GameObject> controllers;
+
+        [SerializeField]
+        private InteractionMode flatScreenInteractionMode;
+
+        [SerializeField]
+        private bool forceModeDetected = false;
+
+        protected ControllerLookup controllerLookup;
+
+
+        public InteractionMode ModeOnDetection => flatScreenInteractionMode;
+
+        /// <inheritdoc />
+        public List<GameObject> GetControllers()
+        {
+            return controllers;
+        }
+
+        public bool IsModeDetected()
+        {
+            return forceModeDetected ||
+                   (!controllerLookup.LeftHandController.currentControllerState.inputTrackingState
+                       .HasPositionAndRotation() && !controllerLookup.RightHandController.currentControllerState
+                       .inputTrackingState.HasPositionAndRotation());
+        }
+
+        protected void Awake()
+        {
+            controllerLookup = ComponentCache<ControllerLookup>.FindFirstActiveInstance();
+        }
+    }
+}
